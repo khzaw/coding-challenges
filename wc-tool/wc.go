85de215cd5b73b2ad975fd95139d9fc9a6be09b5
Kaung Htet <kayhzaw@gmail.com>
2025-01-05 15:22:45 +0800

    ccwc: Rescan individually for each flag

--- wc-tool/wc.go
+++ wc-tool/wc.go
@@ -5,71 +5,84 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"path/filepath"
 )
 
-// -c  - outputs the number of bytes in a file
-// -l  - outputs the number of lines in a file
-// -w  - outputs the number of words in a file
-// -m  - outputs the number of characters in a file
+type Config struct {
+	flagSize  bool
+	flagLines bool
+	flagWords bool
+	flagChars bool
+	f         *os.File
+	size      int
+	lines     int
+	words     int
+	chars     int
+}
 
-func main() {
-	bytesOpt := flag.Bool("c", false, "number of bytes in a file")
-	lineOpt := flag.Bool("l", false, "number of lines in a file")
-	wordOpt := flag.Bool("w", false, "number of words in a file")
-	charOpt := flag.Bool("m", false, "number of characters in a file")
+func (c *Config) count(buf *bufio.Scanner) int {
+	count := 0
+	for buf.Scan() {
+		buf.Text()
+		count += 1
+	}
+	return count
+}
+
+func (c *Config) ScanToken(filename string, splitFn bufio.SplitFunc) int {
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("error opening file: ", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	buf := bufio.NewScanner(f)
+	buf.Split(splitFn)
+	return c.count(buf)
+}
+
+func parseFlags() *Config {
+	// -c  - outputs the number of bytes in a file
+	// -l  - outputs the number of lines in a file
+	// -w  - outputs the number of words in a file
+	// -m  - outputs the number of characters in a file
+	c := flag.Bool("c", false, "number of bytes in a file")
+	l := flag.Bool("l", false, "number of lines in a file")
+	w := flag.Bool("w", false, "number of words in a file")
+	m := flag.Bool("m", false, "number of chars in a file")
 	flag.Parse()
 
+	return &Config{flagSize: *c, flagLines: *l, flagWords: *w, flagChars: *m}
+}
+
+func main() {
+
+	cfg := parseFlags()
+	if !cfg.flagSize && !cfg.flagLines && !cfg.flagWords && !cfg.flagChars {
+		cfg.flagSize = true
+		cfg.flagLines = true
+		cfg.flagWords = true
+	}
+
+	var outputStr string
+
 	if filename := flag.Arg(0); filename != "" {
-		f, err := os.Open(filename)
-		if err != nil {
-			fmt.Println("error opening file: err:", err)
-			os.Exit(1)
-		}
-		defer f.Close()
-
-		path, _ := filepath.Abs(filename)
-
-		fi, err := f.Stat()
-		if err != nil {
-			fmt.Println("error getting file stat: err:", err)
-			os.Exit(1)
+		if cfg.flagLines {
+			outputStr += fmt.Sprintf("     %d", cfg.ScanToken(filename, bufio.ScanLines))
 		}
 
-		if *bytesOpt {
-			fmt.Printf("%d %s\n", fi.Size(), path)
+		if cfg.flagWords {
+			outputStr += fmt.Sprintf("     %d", cfg.ScanToken(filename, bufio.ScanWords))
 		}
 
-		if *lineOpt {
-			lines := 0
-			buf := bufio.NewScanner(f)
-			for buf.Scan() {
-				buf.Text()
-				lines += 1
-			}
-			fmt.Printf("%d %s\n", lines, path)
+		if cfg.flagSize {
+			outputStr += fmt.Sprintf("     %d", cfg.ScanToken(filename, bufio.ScanRunes))
 		}
 
-		if *wordOpt {
-			buf := bufio.NewScanner(f)
-			buf.Split(bufio.ScanWords)
-			words := 0
-			for buf.Scan() {
-				buf.Text()
-				words += 1
-			}
-			fmt.Printf("%d %s\n", words, path)
+		if cfg.flagChars {
+			outputStr += fmt.Sprintf("     %d", cfg.ScanToken(filename, bufio.ScanBytes))
 		}
 
-		if *charOpt {
-			buf := bufio.NewScanner(f)
-			buf.Split(bufio.ScanRunes)
-			chr := 0
-			for buf.Scan() {
-				buf.Text()
-				chr += 1
-			}
-			fmt.Printf("%d %s\n", chr, path)
-		}
+		fmt.Println(outputStr, filename)
 	}
 }
